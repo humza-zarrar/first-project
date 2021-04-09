d836bc1acec28a62dc82c0c55769c172688f82a8
Azan Abrar <azan.abrar@gmail.com>
2021-04-09 17:12:08 +0500

    Basic Angular Concepts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-project';
+  currentVal=""
+  name="angular name"
+  disableboxes= true
+  show= "blue"
+  color="yellow"
+  data= [
+    {
+      name: 'Humza',
+      age: 30,
+      email: "example@example.com",
+      number: "090078601"
+    },
+    {
+      name: 'Nauman',
+      age: 31,
+      email: "example@example.com",
+      number: "090078601"
+    },
+    {
+      name: 'Zain',
+      age: 32,
+      email: "example@example.com",
+      number: "090078601"
+    },
+    {
+      name: 'Adnan',
+      age: 33,
+      email: "example@example.com",
+      number: "090078601"
+    },
+    {
+      name: 'Usman',
+      age: 34,
+      email: "example@example.com",
+      number: "090078601"
+    }
+  ]
+
+  getName()
+  {
+    alert('Humza Zarrar')
+  }
+
+  myEvent(evt: any)
+  {
+    console.warn(evt)
+  }
+
+  getVal(val: any)
+  {
+    console.warn(val)
+    this.currentVal=val
+  }
+
+  enableBoxes()
+  {
+    this.disableboxes=false
+  }
+
+  getUserValue(value: any)
+  {
+    console.warn(value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersModule } from './users/users.module';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UsersModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
